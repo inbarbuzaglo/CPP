ef4730963d8e964735bedc8c105728e9afa39393
InbarBuzaglo <33480360+inbarbuzaglo@users.noreply.github.com>
2018-04-12 01:48:54 +0300

    Update Member.h

--- Member.h
+++ Member.h
@@ -8,7 +8,6 @@ using namespace std;
 static int counter=0;
 static int numID=0;
 
-//The Users class has two vectors of type Users* and a string variable which are used by the Graph class to create the directed Graph.
 class Member{
 
 private:   
@@ -19,8 +18,8 @@ private:
     int id;
     int numOfFollowers;
     int numOfFollowing;
-    vector<Member*>Following; //follow
-    vector<Member*>Followers; //òå÷áéí àçøé
+    vector<Member*>Following; //i follow after him
+    vector<Member*>Followers; //he follows after me
  
 public:
  //Default constructor.
@@ -28,47 +27,36 @@ public:
    //Destructor
      ~Member();
 
-    //Parameterized constructor which initializes the string.
     Member(string name, string password, string gender, int age);
     
-    //Setter method which sets the name of the user.
     void setName(string name);
     
-    //Getter method which gets the name of the user.
     string getName();
 
-    //Setter method which sets the password of the user.
     void setPassword(string password);
 
-    //Getter method which gets the password of the user.
     string getPassword();
 
-    //Setter method which sets the Gender of the user.
     void setGender(string gender);
 
-    //Getter method which gets the Gender of the user.
     string getGender();
 
-    //Setter method which sets the age of the user.
     void setAge(int age);
 
-    //Getter method which gets the age of the user.
     int getAge();
     
-    //Getter method which gets the number of Followers
     int numFollowers(); 
  
-    //Setter method which adds the following to the following vector.
     void follow(Member& u);
 
     void unfollow(Member& u);
     
     void unFollowers(Member& u);
     
-    void newFollowers(Member &m);
+    void newFollowers(Member& u);
     
     int numFollowes();
-     //Getter method which gets the number of Following
+    
     int numFollowing();
 
     static int count();
